ff40d5ae0f44951e8a0e9f22dc7db158d57ef2d4
felixNyalenda <felixasejunior@gmail.com>
2023-05-18 14:45:03 +0300

    completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,31 @@
 ## code your solution here. 
+class Cat
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+end
+
+class Person
+  attr_reader :first_name, :last_name
+
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+end
+
+mercy = Person.new
+mercy.name = 'Beyonce Knowles'
+
+ mercy.first_name
+puts mercy.name
+
+cat = Cat.new
+puts cat.meow
